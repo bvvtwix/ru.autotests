37fadad0455b97dd4898c1b4aeb01b2f84e3457a
Виктор Буцких <no1@MacBook-Pro-Viktor.local>
2022-03-21 21:02:52 +0300

    Initial commit

---
+++ src/test/java/firstTest.java
@@ -0,0 +1,40 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class firstTest {
+    private Logger logger = LogManager.getLogger(firstTest.class);
+
+    WebDriver driver;
+
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        logger.info("driver up");
+    }
+
+    @After
+    public void close(){
+        if (driver != null){
+            driver.close();
+        }
+    }
+
+    @Test
+    public void testLog(){
+        logger.warn("warn - warn level log");
+        logger.info("info - info level log");
+    }
+
+    @Test
+    public void openOtus(){
+        driver.get("https://otus.ru");
+    }
+}
